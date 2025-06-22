e7e46ff4a67d73d3ce8c03d2428861b6b71888cc
Tony Ennis <ennis.tony@gmail.com>
2025-06-22 13:26:39 -0700

    Switch to using base styles repo for base styles docs (pull from cdn)

--- app/controllers/h1rails/demos/base_styles_controller.rb
+++ app/controllers/h1rails/demos/base_styles_controller.rb
@@ -4,15 +4,18 @@ class H1rails::Demos::BaseStylesController < ApplicationController
   before_action :allow_iframe
  
   def index
-    @path = params[:path] || "all"
-    @md = File.read(Rails.root.join("app", "views", "h1rails", "demos", "base_styles", "#{@path}.md"))
-  
-    @content = Utilities.markdown_to_html(@md)
+    get_file
   end
 
   def show 
-    @path = params[:file]
-    @md = File.read(Rails.root.join("app", "views", "h1rails", "demos", "base_styles", "#{@path}.md"))
+    get_file
+  end
+
+  def get_file 
+    doc = params[:file] || "basestyles"
+    url = "https://cdn.base-styles.com/docs/#{doc}.md"
+    response = HTTParty.get(url)
+    @md = response.body
     @content = Utilities.markdown_to_html(@md)
   end
 
--- app/views/h1rails/demos/base_styles/ui-container.md
+++
@@ -1,5 +0,0 @@
-
-
-# .ui-container
-
-Here's how to use this
